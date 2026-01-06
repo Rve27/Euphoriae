52e492b958a155ed6bc2ee7a71ca2cbda4e3650a
Rve27 <rve27github@gmail.com>
2026-01-07 05:55:43 +0700

    gradle: Set compileSdk and targetSdk to Android 16 (36)
    Signed-off-by: Rve27 <rve27github@gmail.com>

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -7,12 +7,12 @@ plugins {
 
 android {
     namespace = "com.oss.euphoriae"
-    compileSdk = 35
+    compileSdk = 36
 
     defaultConfig {
         applicationId = "com.oss.euphoriae"
         minSdk = 29
-        targetSdk = 35
+        targetSdk = 36
         versionCode = 1
         versionName = "1.3"
 
